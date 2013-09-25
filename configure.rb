e43c8616eb83d4d9ad5c51b450dd981ba73f00f5
Sebastian Ospina Berrio <alu0100614213@ull.edu.es>
2013-09-25 17:49:46 +0100

    Terminada Practica

--- configure.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = 'IcJzty40GR9PYIvkZA'
-  config.consumer_secret = 'IHqZEQLzhHXS26X4zq85nXuCjwpHyy9GJ92Fiuhjzk' 
-  config.oauth_token = "1890923737-ScLikNGlVfnEVlP6V4kXd055WFfEzGLqPX12nGa"
-  config.oauth_token_secret = "XugDZCD5Wo1tl7xYXmd7DNBj8MgPRT5AfgEx1Fic"
-end
